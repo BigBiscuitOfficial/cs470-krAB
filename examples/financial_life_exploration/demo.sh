cf4f00f3e8ffb6a5fcfad23dc2c9a8333678c4d9
landon <mannlk@dukes.jmu.edu>
2026-04-26 22:14:31 -0400

    Added demo script with interpretation, gonna add a scaling report

---
+++ examples/financial_life_exploration/demo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+module load mpi
+
+mkdir -p outcomes/
+
+cargo build --release --features "distributed_mpi mpi_verbose_timing"
+
+export FIN_SEED=$(echo $RANDOM) 
+export FIN_HORIZON=60 
+export FIN_REPETITIONS=20
+export FIN_MAX_GENERATION=75
+export FIN_HOUSEHOLDS=48
+export FIN_INDIVIDUALS=128
+
+for i in 1 2 4 8 16 32 64 128
+do
+salloc -Q -n $i mpirun ../target/release/finance_life_exploration | tee runs/finance_life_run_${i}_procs.log
+
+python3 tools/interpret_financial_run.py runs/finance_life_run_${i}_procs.log -o "outcomes/financial_interpretation_${i}_procs.txt"
+
+echo "SEED WAS: ${FIN_SEED}"
+done
+
+
+
+
